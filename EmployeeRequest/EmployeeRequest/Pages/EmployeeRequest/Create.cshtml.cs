561c851d44a075958ece6a1ef6e8ab11c6496fa7
HMD <HMD@DESKTOP-SAFIKLM>
2021-05-03 11:34:56 +0430

    auto generate crud for employRequest

---
+++ EmployeeRequest/EmployeeRequest/Pages/EmployeeRequest/Create.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using EmployeeRequest.Models;
+
+namespace EmployeeRequest.Pages.EmployeeRequest
+{
+    public class CreateModel : PageModel
+    {
+        private readonly EmployeeRequestDBContext _context;
+
+        public CreateModel(EmployeeRequestDBContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["FldEmployeeRequestJobOnetId"] = new SelectList(_context.TblJobs, "FldJobId", "FldJobName");
+        ViewData["FldEmployeeRequestJobTaminId"] = new SelectList(_context.TblJobTamins, "FldTaminJobId", "FldTaminJobCode");
+        ViewData["FldEmployeeRequestUserAccepterId"] = new SelectList(_context.TblEmployeeRequestUsers, "FldEmployeeRequestUserId", "FldEmployeeRequestUserId");
+        ViewData["FldEmployeeRequestUserApplicantId"] = new SelectList(_context.TblEmployeeRequestUsers, "FldEmployeeRequestUserId", "FldEmployeeRequestUserId");
+        ViewData["FldEmployeeRequestUserSubmitterId"] = new SelectList(_context.TblEmployeeRequestUsers, "FldEmployeeRequestUserId", "FldEmployeeRequestUserId");
+            return Page();
+        }
+
+        [BindProperty]
+        public TblEmployeeRequestEmployeeRequest TblEmployeeRequestEmployeeRequest { get; set; }
+
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.TblEmployeeRequestEmployeeRequests.Add(TblEmployeeRequestEmployeeRequest);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ EmployeeRequest/EmployeeRequest/Pages/EmployeeRequest/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using EmployeeRequest.Models;
+
+namespace EmployeeRequest.Pages.EmployeeRequest
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly EmployeeRequestDBContext _context;
+
+        public DeleteModel(EmployeeRequestDBContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public TblEmployeeRequestEmployeeRequest TblEmployeeRequestEmployeeRequest { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            TblEmployeeRequestEmployeeRequest = await _context.TblEmployeeRequestEmployeeRequests
+                .Include(t => t.FldEmployeeRequestJobOnet)
+                .Include(t => t.FldEmployeeRequestJobTamin)
+                .Include(t => t.FldEmployeeRequestUserAccepter)
+                .Include(t => t.FldEmployeeRequestUserApplicant)
+                .Include(t => t.FldEmployeeRequestUserSubmitter).FirstOrDefaultAsync(m => m.FldEmployeeRequestEmployeeRequestId == id);
+
+            if (TblEmployeeRequestEmployeeRequest == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            TblEmployeeRequestEmployeeRequest = await _context.TblEmployeeRequestEmployeeRequests.FindAsync(id);
+
+            if (TblEmployeeRequestEmployeeRequest != null)
+            {
+                _context.TblEmployeeRequestEmployeeRequests.Remove(TblEmployeeRequestEmployeeRequest);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
