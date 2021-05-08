205e7622b40f7097d2d1fada9d5dc7f9a9f2a6f7
HMD <HMD@DESKTOP-SAFIKLM>
2021-05-08 10:05:46 +0430

    complete publish on server section

--- EmployeeRequest/EmployeeRequest/Pages/EmployeeRequest/Transfer.cshtml.cs
+++ EmployeeRequest/EmployeeRequest/Pages/EmployeeRequest/Transfer.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EmployeeRequest.EmployModels;
 using EmployeeRequest.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -13,10 +14,12 @@ namespace EmployeeRequest.Pages.EmployeeRequest
     public class TransferModel : PageModel
     {
         private readonly EmployeeRequestDBContext _context;
+        private readonly EmployDBContext _employContext;
 
-        public TransferModel(EmployeeRequestDBContext context)
+        public TransferModel(EmployeeRequestDBContext context, EmployDBContext employContext)
         {
             _context = context;
+            _employContext = employContext;
         }
 
         public IList<TblEmployeeRequestEmployeeRequest> TblEmployeeRequestEmployeeRequest { get; set; }
@@ -49,7 +52,12 @@ namespace EmployeeRequest.Pages.EmployeeRequest
             {
                 return RedirectToPage("../Index");
             }
-            TblEmployeeRequestEmployeeRequest TblEmployeeRequestEmployeeRequest2 = _context.TblEmployeeRequestEmployeeRequests.Find(id);
+            TblEmployeeRequestEmployeeRequest TblEmployeeRequestEmployeeRequest2 = _context.TblEmployeeRequestEmployeeRequests
+                .Include(t => t.FldEmployeeRequestJobs)
+                .Include(t => t.FldEmployeeRequestJobOnet)
+                .Include(t => t.FldEmployeeRequestJobTamin)
+                .Where(a => a.FldEmployeeRequestEmployeeRequestId == id)
+                .FirstOrDefault();
             if (TblEmployeeRequestEmployeeRequest2 != null)
             {
                 TblEmployeeRequestEmployeeRequest2.FldEmployeeRequestEmployeeRequestIsTransfered = true;
@@ -57,7 +65,32 @@ namespace EmployeeRequest.Pages.EmployeeRequest
                 _context.TblEmployeeRequestEmployeeRequests.Update(TblEmployeeRequestEmployeeRequest2);
                 _context.SaveChanges();
 
-                //TODO: Publish process herev
+                //TODO: Publish process here
+                string jobTitleFrom = null;
+                if (TblEmployeeRequestEmployeeRequest2.FldEmployeeRequestJobTitleFromId == 1)
+                {
+                    jobTitleFrom = TblEmployeeRequestEmployeeRequest2.FldEmployeeRequestJobs.JobsName;
+                }
+                else if (TblEmployeeRequestEmployeeRequest2.FldEmployeeRequestJobTitleFromId == 2)
+                {
+                    jobTitleFrom = TblEmployeeRequestEmployeeRequest2.FldEmployeeRequestJobTamin.FldTaminJobName;
+                }
+                else
+                {
+                    jobTitleFrom = TblEmployeeRequestEmployeeRequest2.FldEmployeeRequestJobOnet.FldJobName;
+                }
+                EmployModels.TblJob newJob = new EmployModels.TblJob()
+                {
+                    JobTitle = jobTitleFrom,
+                    IsActive = true,
+                    StartDate = TblEmployeeRequestEmployeeRequest2.FldEmployeeRequestEmployeeRequestStartDate,
+                    EndDate = TblEmployeeRequestEmployeeRequest2.FldEmployeeRequestEmployeeRequestEndDate,
+                    NeedMan = TblEmployeeRequestEmployeeRequest2.FldEmployeeRequestEmployeeRequestNeedMan,
+                    NeedWoman = TblEmployeeRequestEmployeeRequest2.FldEmployeeRequestEmployeeRequestNeedWoman,
+                    Description = TblEmployeeRequestEmployeeRequest2.FldEmployeeRequestEmployeeRequestJobDescription
+                };
+                _employContext.TblJobs.Add(newJob);
+                _employContext.SaveChanges();
             }
             return RedirectToPage("Index");
         }
