20871df8f5f58b3d83267d1cff58749b95099022
HMD <HMD@DESKTOP-SAFIKLM>
2021-05-12 16:14:15 +0430

    make filter

--- EmployeeRequest/EmployeeRequest/Pages/EmployAccept/Search.cshtml.cs
+++ EmployeeRequest/EmployeeRequest/Pages/EmployAccept/Search.cshtml.cs
@@ -34,5 +34,25 @@ namespace EmployeeRequest.Pages.EmployAccept
         {
             return new JsonResult(new { data = _context.TblEmployeeRequestEmployees.Include(a => a.TblEmployeeRequestPrimaryInformations).Select(a => new { a.FldEmployeeRequestEmployeeId, a.TblEmployeeRequestPrimaryInformations.FirstOrDefault().FldEmployeeRequestPrimaryInformationFirstName, a.TblEmployeeRequestPrimaryInformations.FirstOrDefault().FldEmployeeRequestPrimaryInformationLastName, a.TblEmployeeRequestPrimaryInformations.FirstOrDefault().FldEmployeeRequestPrimaryInformationNationalCode, a.TblEmployeeRequestPrimaryInformations.FirstOrDefault().FldEmployeeRequestPrimaryInformationPhoneNo }).ToList() });
         }
+
+        public async Task<IActionResult> OnGetFilterSearchAsync(bool active_gender,string gender)
+        {
+            var result = _context.TblEmployeeRequestEmployees
+                .Include(a => a.TblEmployeeRequestPrimaryInformations)
+                .Where(_=>true);
+
+            if (active_gender)
+            {
+                result = result.Where(a => a.TblEmployeeRequestPrimaryInformations.FirstOrDefault().FldEmployeeRequestPrimaryInformationGender == gender);
+            }
+
+            var finalResult = result.Select(a => new { a.FldEmployeeRequestEmployeeId, a.TblEmployeeRequestPrimaryInformations
+                .FirstOrDefault().FldEmployeeRequestPrimaryInformationFirstName, a.TblEmployeeRequestPrimaryInformations
+                .FirstOrDefault().FldEmployeeRequestPrimaryInformationLastName, a.TblEmployeeRequestPrimaryInformations
+                .FirstOrDefault().FldEmployeeRequestPrimaryInformationNationalCode, a.TblEmployeeRequestPrimaryInformations
+                .FirstOrDefault().FldEmployeeRequestPrimaryInformationPhoneNo }).ToList();
+
+            return new JsonResult(new { data = finalResult });
+        }
     }
 }
