a86ed0046ca611799bacdd8e463041e921f02b34
HMD <HMD@DESKTOP-SAFIKLM>
2021-05-12 13:04:44 +0430

    add search

---
+++ EmployeeRequest/EmployeeRequest/Pages/EmployAccept/Search.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeRequest.Models;
+using Microsoft.Ajax.Utilities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeRequest.Pages.EmployAccept
+{
+    public class SearchModel : PageModel
+    {
+        private readonly EmployeeRequestDBContext _context;
+
+        public SearchModel(EmployeeRequestDBContext context)
+        {
+            _context = context;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnGetSearchAsync()
+        {
+            return new JsonResult(_context.TblEmployeeRequestEmployees.Include(a => a.TblEmployeeRequestPrimaryInformations).Select(a => new { a.FldEmployeeRequestEmployeeId, a.TblEmployeeRequestPrimaryInformations.FirstOrDefault().FldEmployeeRequestPrimaryInformationFirstName, a.TblEmployeeRequestPrimaryInformations.FirstOrDefault().FldEmployeeRequestPrimaryInformationLastName, a.TblEmployeeRequestPrimaryInformations.FirstOrDefault().FldEmployeeRequestPrimaryInformationNationalCode, a.TblEmployeeRequestPrimaryInformations.FirstOrDefault().FldEmployeeRequestPrimaryInformationPhoneNo }).ToList());
+        }
+    }
+}
